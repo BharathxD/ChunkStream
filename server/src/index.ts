8c762de39151db76ed62079fc998ee4f0d6a7e82
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-10 20:34:52 +0530

    Configured pino for logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import logger from "../utils/logger";
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  logger.info(`Server is running on http://localhost:${PORT}`);
 });
 
 //? These are signals used to terminate a process
@@ -15,7 +16,7 @@ const gracefulShutdown = (signal: string) => {
   process.on(signal, () => {
     server.close();
     // TODO: Disconnect from the DataBase
-    console.log(" My Work Here Is Done ✅");
+    logger.info("My Work Here Is Done ✅");
     process.exit(0);
   });
 };
