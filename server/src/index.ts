169c2e42a0fa88dadf71e084e75a886efbf0f039
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-10 20:22:12 +0530

    Initialized server and implemented graceful shutdown

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,23 @@
 import express from "express";
 
 const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+const signals = ["SIGTERM", "SIGINT"];
+
+const gracefulShutdown = (signal: string) => {
+  process.on(signal, () => {
+    server.close();
+    // TODO: Disconnect from the DataBase
+    process.exit(0);
+  });
+};
+
+for (let i = 0; i < signals.length; i++) {
+  gracefulShutdown(signals[i]);
+}
