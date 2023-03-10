25b37788be5ffaae5df6871a7f3f5664ec1faea8
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-10 21:08:57 +0530

    Misc Fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import logger from "../utils/logger";
+import logger from "./utils/logger";
+import { connect, disconnect } from "./utils/connect";
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, async () => {
+  await connect();
   logger.info(`Server is running on http://localhost:${PORT}`);
 });
 
@@ -13,9 +15,10 @@ const server = app.listen(PORT, () => {
 const signals = ["SIGTERM", "SIGINT"];
 
 const gracefulShutdown = (signal: string) => {
-  process.on(signal, () => {
+  process.on(signal, async () => {
     server.close();
-    // TODO: Disconnect from the DataBase
+    await disconnect();
+    //? Disconnect from the Database
     console.log("Exit: My Work Here Is Done ✅");
     process.exit(0);
   });
--- server/src/utils/connect.ts
+++ server/src/utils/connect.ts
@@ -4,8 +4,9 @@ import logger from "./logger";
 const DB_CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/video-stream";
 
-const connect = async () => {
+export const connect = async () => {
   try {
+    mongoose.set("strictQuery", false);
     logger.info("Connecting to the Database...");
     await mongoose.connect(DB_CONNECTION_STRING);
     logger.info("Sucessfully connected to the Database ✅");
@@ -15,12 +16,14 @@ const connect = async () => {
   }
 };
 
-const disconnect = async () => {
+export const disconnect = async () => {
   try {
     logger.info("Terminating the Connection to the Database...");
     await mongoose.connection.close();
     logger.info("Sucessfully Disconnected from the Database ✅");
   } catch (error: any) {
     logger.error(error);
+  } finally {
+    return;
   }
 };
