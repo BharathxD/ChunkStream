efffd16a0075a75fc8f25b4063df431dc9a09c3e
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-10 20:29:10 +0530

    Workedon signals

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,12 +8,14 @@ const server = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+//? These are signals used to terminate a process
 const signals = ["SIGTERM", "SIGINT"];
 
 const gracefulShutdown = (signal: string) => {
   process.on(signal, () => {
     server.close();
     // TODO: Disconnect from the DataBase
+    console.log(" My Work Here Is Done ✅");
     process.exit(0);
   });
 };
