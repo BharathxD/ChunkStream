9454562f10d794a48096159f22d06c5699942869
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-11 18:48:30 +0530

    Worked on pre-save hook on user model

---
+++ server/src/modules/user/user.model.ts
@@ -0,0 +1,29 @@
+import { getModelForClass, prop, pre } from "@typegoose/typegoose";
+import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import argon2 from "argon2";
+
+@pre<User>("save", async function (this, next) {
+  // If the password is bening modified and the password is new, we will hash it
+  if (!this.isModified("password") || this.isNew) {
+    // We don't need to salt, as argon will handle it
+    const hash = await argon2.hash(this.password);
+    this.password = hash;
+    return next();
+  }
+})
+
+// We are going to use this class for the model interface, and export a model
+export class User {
+  @prop({ required: true, unique: true })
+  public username: string;
+  @prop({ required: true, unique: true })
+  public email: string;
+  @prop({ required: true })
+  public password: string;
+}
+
+export const UserModel = getModelForClass(User, {
+  schemaOptions: {
+    timestamps: true,
+  },
+});
