d2bb407e361bc6986e1bbe6f81789439fcb5d62d
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-11 19:12:35 +0530

    Created a controller function to create user

---
+++ server/src/modules/user/user.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { createUser } from "./user.service";
+
+export const registerUserHandler = async (
+  request: Request,
+  response: Response
+) => {
+  const { uesrname, email, password } = request.body;
+  try {
+    // TODO: Create User
+    const user = await createUser({ username, email, password });
+    response.status(StatusCodes.CREATED).send("User created successfully");
+  } catch (error: any) {
+    if (error.code === 11000) {
+      return response
+        .status(StatusCodes.CONFLICT)
+        .send({ message: "User already exists" });
+    } else {
+      return response
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send({ message: error.message });
+    }
+  }
+};
---
+++ server/src/modules/user/user.service.ts
@@ -0,0 +1,5 @@
+import { User } from "./user.model";
+
+export const createUser = async (user: User) => {
+    
+};
