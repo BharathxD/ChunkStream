e4dbde2cee62022fab749fdd11767f7568c91701
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-11 23:28:38 +0530

    Finished working on auth controller

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -2,19 +2,30 @@ import { Request, Response } from "express";
 import { findUserByEmail } from "../user/user.service";
 import { User } from "../user/user.model";
 import { StatusCodes } from "http-status-codes";
+import { signJWT } from "./auth.utils";
 
 export const loginHandler = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   // TODO: Find user by email
   const user = findUserByEmail(email);
+  // TODO: Verify user password
   if (!user || !user.comparePassword(password)) {
     return res
       .status(StatusCodes.UNAUTHORIZED)
       .send({ message: "Invalid email or password " });
   }
   const payload = user.toJSON();
-  // TODO: Verify user password
   // TODO: Sign a JWT
+  const jwt = signJWT(payload);
   // TODO: Add a cookie to response
+  res.cookie("accessToken", jwt, {
+    maxAge: 3.154e10, //? 1 Year
+    httpOnly: true,
+    domain: "localhost", //? Dev Environment
+    path: "/",
+    sameSite: "strict",
+    secure: false, //? Dev Environment
+  });
   // TODO: RESPOND
+  return res.status(StatusCodes.OK).send(jwt);
 };
--- server/src/modules/auth/auth.utils.ts
+++ server/src/modules/auth/auth.utils.ts
@@ -3,21 +3,21 @@ import logger from "../../utils/logger";
 
 export const signJWT = (
   payload: string | Buffer | object,
-  keyName: "accessTokenPrivateKey",
   options?: jwt.SignOptions
 ) => {
-  const PrivateKey = process.env.ACCESS_TOKEN_PRIVATE_KEY;
-  const expiresIn = process.env.EXPIRES_IN || "7d";
-  const signingKey = Buffer.from(PrivateKey!, "base64").toString("ascii");
+  const JWT_SECRET = process.env.ACCESS_TOKEN_PRIVATE_KEY;
+  const EXPIRES_IN = process.env.EXPIRES_IN || "7d";
+  const signingKey = Buffer.from(JWT_SECRET!, "base64").toString("ascii");
   return jwt.sign(payload, signingKey, {
     ...(options && options),
     algorithm: "RS256",
+    expiresIn: EXPIRES_IN,
   });
 };
 
-export const verifyJWT = (token: string, keyName: "accessTokenPublicKey") => {
-  const PubicKey = process.env.ACCESS_TOKEN_PRIVATE_KEY;
-  const verifyingKey = Buffer.from(PubicKey!, "base64").toString("ascii");
+export const verifyJWT = (token: string) => {
+  const JWT_SECRET = process.env.ACCESS_TOKEN_PRIVATE_KEY;
+  const verifyingKey = Buffer.from(JWT_SECRET!, "base64").toString("ascii");
   try {
     const decoded = jwt.verify(token, verifyingKey);
     return {
