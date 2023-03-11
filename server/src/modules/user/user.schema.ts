419a3cb388491802f812e25ad16cddbf306e93cd
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-11 19:20:24 +0530

    Worked on user schema

---
+++ server/src/modules/user/user.schema.ts
@@ -0,0 +1,26 @@
+import { object, string } from "zod";
+
+export const RegisterUserSchema = object({
+  user: object({
+    username: string({
+      required_error: "Username is required",
+    }),
+    email: string({
+      required_error: "Email is required",
+    }).includes("@"),
+    password: string({
+      required_error: "Password is Required",
+    })
+      .min(6, "The length of the password should be atleast 6 Characters long")
+      .max(
+        64,
+        "Password is too long, it should not be longer than 64 Characters"
+      ),
+    confirmPassword: string({
+      required_error: "Password confirmation is Required",
+    }),
+  }).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  }),
+});
