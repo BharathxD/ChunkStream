3f646984b1d1453095e6225a43e36897484fa597
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-12 11:15:19 +0530

    Tested the API using postman

--- server/src/modules/user/user.route.ts
+++ server/src/modules/user/user.route.ts
@@ -2,8 +2,13 @@ import express from "express";
 import { registerUserHandler } from "./user.controller";
 import { processRequestBody } from "zod-express-middleware";
 import { RegisterUserSchema } from "./user.schema";
+import requireUser from "../../middleware/requireUser";
 
 const router = express.Router();
+
+router.get("/", requireUser, (req, res) => {
+  res.send(res.locals.user);
+})
 
 router.post(
   "/",
