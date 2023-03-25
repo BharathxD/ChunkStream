f23f83017397708ad762a23b432ac4c3ada02f81
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-25 19:27:42 +0530

    refactor: Optimize verifyAccessToken middleware function

--- server/src/middleware/deserialize.user.ts
+++ server/src/middleware/deserialize.user.ts
@@ -2,18 +2,18 @@ import { Request, Response, NextFunction } from "express";
 import { verifyJWT } from "../modules/auth/auth.utils";
 
 const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
-  const accessToken = (
-    req.headers.authorization ||
-    req.cookies.accessToken ||
-    ""
-  ).replace(/^Bearer\s/, "");
+  const accessToken =
+    req.headers.authorization?.replace(/^Bearer\s/, "") ||
+    req.cookies.accessToken;
   if (!accessToken) {
-    return next();
+    return res.status(401).json({ message: "Access token missing" });
   }
-  const decoded = verifyJWT(accessToken);
-  if (decoded) {
+  try {
+    const decoded = verifyJWT(accessToken);
     res.locals.user = decoded;
-    return next();
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid access token" });
   }
 };
 
