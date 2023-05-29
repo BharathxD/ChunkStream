b5fc7197e3724510fd773031482a8d4c1354a80a
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-29 16:36:11 +0530

    Add React-Video Player

--- client/pages/watch/[videoId].tsx
+++ client/pages/watch/[videoId].tsx
@@ -1,20 +1,35 @@
+import HomePageLayout from "@/layout/HomePageLayout";
+import { Card } from "@mantine/core";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { Fragment, ReactElement } from "react";
+import ReactPlayer from "react-player";
 
 const Watch = () => {
   const { query } = useRouter();
+  const videoSrc = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/videos/${query.videoId}`;
+  if (!query.videoId) {
+    return null;
+  }
   return (
-    <>
-      <video
-        src={`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/videos/${query.videoId}`}
-        width={800}
-        height={"auto"}
-        controls
-        autoPlay
-        id="video-player"
-      ></video>
-    </>
+    <Card w="100%" h="100vh" display="flex" padding={"2rem"}>
+      <Card>
+        <ReactPlayer
+          style={{
+            aspectRatio: "16/9",
+            borderRadius: "1rem",
+          }}
+          url={videoSrc}
+          controls={true}
+          playing={false}
+          muted={false}
+        />
+      </Card>
+    </Card>
   );
+};
+
+Watch.getLayout = function (page: ReactElement) {
+  return <HomePageLayout>{page}</HomePageLayout>;
 };
 
 export default Watch;
